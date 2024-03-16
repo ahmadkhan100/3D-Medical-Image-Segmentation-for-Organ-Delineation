a252cc65d3200e563d048d33e6f9522968e6775f
ahmadkhan100 <163046609+ahmadkhan100@users.noreply.github.com>
2024-03-16 16:15:46 -0400

    Update optimizer.py

--- model/optimizer.py
+++ model/optimizer.py
@@ -1,1 +1,19 @@
+# model/optimizer.py
+import tensorflow as tf
+
+def optimize_model(model, save_path):
+    # Convert Keras model to a TensorFlow Lite model
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    tflite_model = converter.convert()
+    
+    # Save the model
+    with open(save_path, 'wb') as f:
+        f.write(tflite_model)
+
+# Usage
+# Provide path to save the optimized model
+optimized_model_path = 'path/to/save/optimized_model.tflite'
+
+# Assuming 'model' is already created using the unet_3d.py script
+optimize_model(model, optimized_model_path)
 
